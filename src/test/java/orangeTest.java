72e5bd78d147522e8ea896117343a1e6b9a9a929
admin <romain.clement@cimpa.com>
2020-11-16 16:11:39 +0100

    ffff

--- src/test/java/orangeTest.java
+++ src/test/java/orangeTest.java
@@ -1,9 +1,8 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class OrangeTest {
+class orangeTest {
 
 	@Test
 	void testPanierIsEquals() throws Exception {
