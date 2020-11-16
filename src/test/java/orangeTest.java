71621e980c7361fd41f0c5f06b1e3fbad80f5612
admin <romain.clement@cimpa.com>
2020-11-16 16:04:04 +0100

    modif test

--- src/test/java/orangeTest.java
+++ src/test/java/orangeTest.java
@@ -1,10 +1,51 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class orangeTest {
+import org.junit.jupiter.api.Test;
 
-	@org.junit.Test
-	public void test() {
-		
+class OrangeTest {
+
+	@Test
+	void testPanierIsEquals() throws Exception {
+		Orange orange = new Orange(0.2, "France");
+		Orange orange2 = new Orange(0.5, "Espagne");
+		Panier panier = new Panier();
+		Panier panier2 = new Panier();
+		panier.ajoute(orange);
+		panier.ajoute(orange2);
+		panier2.ajoute(orange);
+		panier2.ajoute(orange2);
+
+		assertEquals(true, panier.equals(panier2));
+	}
+
+	@Test
+	void testPanierIsNotEquals() throws Exception {
+		Orange orange = new Orange(0.2, "France");
+		Orange orange2 = new Orange(0.5, "Espagne");
+		Panier panier = new Panier();
+		Panier panier2 = new Panier();
+		panier.ajoute(orange);
+		panier.ajoute(orange2);
+		panier2.ajoute(orange);
+
+		assertEquals(false, panier.equals(panier2));
+	}
+
+	@Test
+	void testOrangeIsEquals() throws Exception {
+		Orange orange = new Orange(0.2, "France");
+		Orange orange2 = new Orange(0.2, "France");
+
+		assertEquals(true, orange.equals(orange2));
+	}
+
+	@Test
+	void testOrangeIsNotEquals() throws Exception {
+		Orange orange = new Orange(0.2, "France");
+		Orange orange2 = new Orange(0.5, "Espagne");
+
+		assertEquals(false, orange.equals(orange2));
 	}
 
 }
